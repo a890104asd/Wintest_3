52ec4eea0259ea9b1a45afd257a918ee7bc32a4f
a890104asd <a890104asd@github.com>
2022-06-16 19:16:40 +0800

    定贏傳媒測驗3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 10;
+            int[] num = new int[N];
+            int i;
+            Console.WriteLine("輸入10個數字");
+            try
+            {
+                for (i = 0; i < num.Length; i++){
+                    num[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (i = 0; i < N; i++)
+                {
+                    for(int j = i + 1; j < N; j++)
+                    {
+                        if (num[j] < num[i])
+                        {
+                            int temp = num[i];
+                            num[i] = num[j];
+                            num[j] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine("結果：");
+                foreach(int temp in num){
+                    Console.Write("{0,-4}", temp);
+                    
+                }
+                Console.ReadLine();
+
+            }
+            catch(Exception e)
+            {
+                Console.Write("錯誤：{0}",e);
+            }
+        }
+    }
+}
